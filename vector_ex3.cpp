7f1c414f89b22cf38d1b7c8e4eb769985e6418d1
Paula Cruz <47175612+paulacz@users.noreply.github.com>
2020-02-27 22:11:32 -0300

    Update vector_ex3.cpp

--- vector_ex3.cpp
+++ vector_ex3.cpp
@@ -1,13 +1,35 @@
 /* IPOG - Engenharia Civil
    Paula Cruz - 02/2020
    Vetor
-   ex.3.: Faça um programa que leia 10 números positivos e
+   ex.3.: FaÃ§a um programa que leia 10 nÃºmeros positivos e
    armazene em um vetor, depois calcule e mostre o maior e 
-   o menor número.
+   o menor nÃºmero.
    
 */
 
 #include <stdio.h>
+#define tam 10
 
 int main() {
+	float valor [tam], menor = -1, maior = -1;
+	int i;
+	
+	for (i=0; i<tam; i++){
+		printf("Digite o %i valor...:", i + 1);scanf("%f", &valor[i]);
+		if (valor[i]<0){
+			printf("Valor negativo nao permitido\n");
+			printf("Digite o %i valor...:", i+1);scanf("%f",&valor[i]);
+		}
+		if (i==0){menor=valor[i];}
+		if (valor[i] < menor){
+		menor = valor[i];}
+		else{
+		    if(valor[i]> maior){
+			maior = valor[i];
+			}
+			}
+		}
+	printf("\nO menor valor e...:%.1f\n", menor);
+	printf("\nO maior valor e...:%.1f\n", maior);
+	}
 	
