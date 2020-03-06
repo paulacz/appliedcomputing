dff54e397f43b0c5838ff0da168e69beb8a45e7a
Paula Cruz <47175612+paulacz@users.noreply.github.com>
2020-03-05 22:14:03 -0300

    Rename vector_ex1.cpp to vector/vector_ex1.cpp

---
+++ vector/vector_ex1.cpp
@@ -0,0 +1,14 @@
+  
+/* IPOG - Engenharia Civil
+   Paula Cruz - 02/2020
+   Vetor
+   ex.1.: Faça um programa que leia o salário bruto de 15 (quinze) 
+   funcionários e armazene em um vetor, lembrando-se que não existe 
+   salário negativo. Depois calcule e escreva a média salarial e quantos 
+   funcionários têm o salário bruto maior que a média salarial.
+*/
+
+#include <stdio.h>
+
+int main() {
+	
